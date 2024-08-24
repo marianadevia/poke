243d4ec7557fedade75cd5c962914534aec1eba1
Mariana Devia <39686829+marianadevia@users.noreply.github.com>
2024-08-24 15:02:58 -0600

    feat(feature-domain-layer) Replace retrofit and inject it through dependency injection

--- app/src/main/java/com/example/pokemon/data/datasource/remote/PokemonAPI.kt
+++ app/src/main/java/com/example/pokemon/data/datasource/remote/PokemonAPI.kt
@@ -3,10 +3,7 @@ package com.example.pokemon.data.datasource.remote
 import com.example.pokemon.data.datasource.remote.model.AbilityResponseApiModel
 import com.example.pokemon.data.datasource.remote.model.PokemonDetailResponseApiModel
 import com.example.pokemon.data.datasource.remote.model.PokemonResponseApiModel
-import com.google.gson.GsonBuilder
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -19,14 +16,4 @@ interface PokemonAPI {
 
     @GET("ability/{id}")
     suspend fun getAbilityDetail(@Path("id") id: Int): Response<AbilityResponseApiModel>
-}
-
-
-object RetrofitClient {
-    private const val BASE_URL = "https://pokeapi.co/api/v2/"
-
-    val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
 }
---
+++ app/src/main/java/com/example/pokemon/di/PokemonDataModule.kt
@@ -0,0 +1,29 @@
+package com.example.pokemon.di
+
+import com.example.pokemon.data.datasource.remote.PokemonAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class PokemonDataModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokemonApi(retrofit: Retrofit): PokemonAPI {
+        return retrofit.create(PokemonAPI::class.java)
+    }
+}
